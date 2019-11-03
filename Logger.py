d069089cd3cd556045acb84de13584dbc02d8a84
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-04 00:22:36 +0100

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify, request, Response
 import json
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 print(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
 
 hids_entities = [
     {
