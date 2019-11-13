7c71991d51c2a3be6c7663c8cc038b0ba9fffd41
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-13 14:19:18 +0100

    fixed database

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-
+import pprint
+"""
+{
+    "user" : {"name" : "Thomas", "guid" : "1337x"},
+    "computer" : {"name" : "DESKTOP-1337"},
+    "file" : {"name" : "virus.exe", "path":"/roet", "hash" : "hhhh"  }
+}
+"""
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -14,81 +21,71 @@ db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
-# Hids Class/Model
-class Hids(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), unique=True)
-  file = db.Column(db.String(200))
-  hash = db.Column(db.String(200))
 
-  def __init__(self, name, file, hash):
-    self.name = name
-    self.file = file
-    self.hash = hash
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False)
+    guid = db.Column(db.String(120), unique=False)
 
-# Hids Schema
-class HidsSchema(ma.Schema):
-  class Meta:
-    fields = ('id', 'name', 'file', 'hash')
+    def __init__(self, user):
+        self.name = user["name"]
+        self.guid = user["guid"]
 
-# Init schema
-hids_schema = HidsSchema
-hidss_schema = HidsSchema
 
-# Create a Hids
-@app.route('/hids', methods=['POST'])
-def add_hids():
-  name = request.json['name']
-  file = request.json['file']
-  hash = request.json['hash']
-  qty = request.json['qty']
+class Computer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False)
+    guid = db.Column(db.String(120), unique=False)
 
-  new_hids = Hids(name, file, hash)
+    def __init__(self, computer):
+        self.name = computer["name"]
+        # self.guid = computer["guid"]
 
-  db.session.add(new_hids)
-  db.session.commit()
 
-  return hids_schema.jsonify(new_hids)
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(80), unique=False)
+    name = db.Column(db.String(120), unique=False)
+    hash = db.Column(db.String(300), unique=False)
 
-# Get All Hids
-@app.route('/hids', methods=['GET'])
-def get_hidss():
-  all_hidss = Hids.query.all()
-  result = hidss_schema.dump(all_hidss)
-  return jsonify(result.data)
+    def __init__(self, file):
+        self.path = file["path"]
+        self.name = file["name"]
+        self.hash = file["hash"]
 
-# Get Single Hids
-@app.route('/hids/<id>', methods=['GET'])
-def get_hids(id):
-  hids = Hids.query.get(id)
-  return hids_schema.jsonify(hids)
 
-# Update a hids
-@app.route('/hids/<id>', methods=['PUT'])
-def update_hids(id):
-  hids = Hids.query.get(id)
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('user', 'computer', 'file')
 
-  name = request.json['name']
-  file = request.json['file']
-  hash = request.json['hash']
 
-  hids.name = name
-  hids.file = file
-  hids.hash = hash
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
-  db.session.commit()
 
-  return hids_schema.jsonify(hids)
+# endpoint to create new user
+@app.route("/hids", methods=["POST"])
+def add_user():
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(request.__dict__)
+    user = request.json['user']
+    computer = request.json['computer']
+    file = request.json['file']
 
-# Delete Hids
-@app.route('/hids/<id>', methods=['DELETE'])
-def delete_hids(id):
-  hids = Hids.query.get(id)
-  db.session.delete(hids)
-  db.session.commit()
+    new_user = User(user)
+    new_computer = Computer(computer)
+    new_file = File(file)
+    db.session.add(new_user)
+    db.session.add(new_computer)
+    db.session.add(new_file)
+    db.session.commit()
+    return("ok")
+    # return jsonify({"user": new_user, "computer": new_computer, "file": new_file})
 
-  return hids_schema.jsonify(hids)
+
 
 # Run Server
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
+
