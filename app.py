0a4f1c080b846e8ea576b4c58817e6c7b322a179
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-14 00:59:23 +0100

    added conf file

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 from settings import *
 from UserModel import User, Computer, File, db
-import pprint
 
 # endpoint to create new user
 @app.route("/hids", methods=["POST"])
 def add_user():
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(request.__dict__)
+
     user = request.json['user']
     computer = request.json['computer']
     file = request.json['file']
@@ -22,6 +21,29 @@ def add_user():
     return("ok")
     # return jsonify({"user": new_user, "computer": new_computer, "file": new_file})
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=["GET", 'POST'])
+def upload():
+    if request.method == 'POST':
+        uploaded_file = request.files['File']
+
+        if uploaded_file.filename != 'conf.cfg':
+            print('Geen geldig config')
+            return ("", 204)
+
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            filename = secure_filename(uploaded_file.filename)
+            uploaded_file.save(os.path.join("conf", filename))
+            print('nieuwe config geupload')
+            return ("", 204)
+
+    if request.method == 'GET':
+        return
 
 
 # Run Server
