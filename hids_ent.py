397f5ee389eda078b89f48c35972cde822e39bea
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-15 14:06:09 +0100

    Update hids_ent.py

--- hids_ent.py
+++ hids_ent.py
@@ -2,6 +2,6 @@ hids_entities = [
 {
     "user" : {"name" : "Thomas", "guid" : "1337x"},
     "computer" : {"name" : "DESKTOP-1337"},
-    "file" : {"name" : "virus.exe", "path":"/root", "hash" : "hhhh"  }
+    "file" : {"name" : "OOo_2.0.1_LinuxIntel_install.tar.gz", "path":"/home/thomas", "hash" : "e0d123e5f316bef78bfdf5a008837577"  }
 }
 ]
