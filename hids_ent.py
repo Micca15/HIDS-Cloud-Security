fdc99ff99a24b8f2b6150844867322da13a9054a
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-15 13:59:30 +0100

    changed conf

--- hids_ent.py
+++ hids_ent.py
@@ -2,6 +2,6 @@ hids_entities = [
 {
     "user" : {"name" : "Thomas", "guid" : "1337x"},
     "computer" : {"name" : "DESKTOP-1337"},
-    "file" : {"name" : "virus.exe", "path":"/roet", "hash" : "hhhh"  }
+    "file" : {"name" : "virus.exe", "path":"/root", "hash" : "hhhh"  }
 }
 ]
--- settings.py
+++ settings.py
@@ -8,4 +8,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Allowed extensions for the config file
 ALLOWED_EXTENSIONS = {'cfg'}
