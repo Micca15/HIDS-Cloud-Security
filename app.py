7362ac3617cd294ecb1173b11067b00141e8b995
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-18 21:52:25 +0100

    fixes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask_restful import Resource, reqparse
 from flask import request, send_from_directory
 from werkzeug.utils import secure_filename
 from settings import *
@@ -5,22 +6,28 @@ from models.user import User
 from models.computer import Computer
 from models.file import File
 
+
+# create db with the tables in models
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 # endpoint to create new user
 @app.route("/hids", methods=["POST"])
 def add_user():
-
     user = request.json['user']
     computer = request.json['computer']
     file = request.json['file']
-
     new_user = User(user)
     new_computer = Computer(computer)
     new_file = File(file)
+
     db.session.add(new_user)
     db.session.add(new_computer)
     db.session.add(new_file)
     db.session.commit()
-    return("ok")
+    return ("ok", 200)
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -45,9 +52,10 @@ def upload():
     if request.method == 'GET':
         return send_from_directory(os.path.join('conf'), 'conf.cfg', as_attachment=True)
 
+
 # Run Server
 if __name__ == '__main__':
     from db import db
+
     db.init_app(app)
     app.run(port=9000, debug=True)
-
