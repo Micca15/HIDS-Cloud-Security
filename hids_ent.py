d2ed31b5b5f966416ccffd6d89318e62ebc3353d
Micca15 <57052404+Micca15@users.noreply.github.com>
2019-11-22 01:35:37 +0100

    Update hids_ent.py

--- hids_ent.py
+++ hids_ent.py
@@ -4,22 +4,22 @@ hids_entities = [
         "computer": "DESKTOP-05LUT9P",
         "files": [{
             "path": "C: \\Users\\Danesh\\Desktop",
-            "file": "CodeBlocks.lnk",
+            "name": "CodeBlocks.lnk",
             "hash": "9fd52d4f9425ae2ae22f8f5d164691f2"
         },
             {
                 "path": "C: \\Users\\Danesh\\Desktop",
-                "file": "desktop.ini",
+                "name": "desktop.ini",
                 "hash": "8f303c9e28ebe10636360f3212e5d16b"
             },
             {
                 "path": "C: \\Users\\Danesh\\Desktop",
-                "file": "Discord.lnk",
+                "name": "Discord.lnk",
                 "hash": "2d13ddfe114b12a7a1f4e977a0c0a1e8"
             },
             {
                 "path": "C: \\Users\\Danesh\\Desktop",
-                "file": "ida64.exe-Shortcut.lnk",
+                "name": "ida64.exe-Shortcut.lnk",
                 "hash": "af23f755e4f5a2681037a551a5957176"
             }]
     }
